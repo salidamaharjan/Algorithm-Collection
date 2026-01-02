d6795680bb8bfd1a7d36a62215cef771ac2e38af
Salida Maharjan <salidamaharjan@outlook.com>
2026-01-02 01:10:37 -0600

    Solve rotate image

--- algojava/A009RotateImage.java
+++ algojava/A009RotateImage.java
@@ -1,27 +1,34 @@
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class A009RotateImage {
     public static void rotate(int[][] matrix) {
         int len = matrix.length;
+        int arrayLen = matrix[0].length;
+//        System.out.println("arrayLen: " + arrayLen);
 
-        Arrays.sort(matrix, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] a, int[] b) {
-                return b[0] - a[0];
+        int[][] rotatedMatrix = new int[len][arrayLen];
+
+        for (int arrayIndex = 0; arrayIndex < len; arrayIndex++) {
+            int index = len - 1;
+            for (int elementIndex = 0; elementIndex < arrayLen; elementIndex++) {
+                rotatedMatrix[arrayIndex][elementIndex] = matrix[index][arrayIndex];
+                index--;
             }
-        });
-        for (int[] ints : matrix) {
-            System.out.println(
-                    ints[0] + ", " + ints[1] + ", " + ints[2] + ", " + ints[3]
-            );
+        }
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < arrayLen; j++) {
+                matrix[i][j] = rotatedMatrix[i][j];
+            }
         }
 
     }
 
     public static void main(String[] args) {
-        int[][] matrix1 = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        int[][] matrix1 = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        int[][] matrix2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         rotate(matrix1);
+        rotate(matrix2);
     }
 }
 //Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
